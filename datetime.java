1ced59b4bf87af9ee331a7bb72a861d8107a8231
Garima73 <44046176+Garima73@users.noreply.github.com>
2020-09-27 10:00:32 +0545

    Add files via upload

---
+++ datetime.java
@@ -0,0 +1,11 @@
+package practice;
+import java.util.Date; 
+
+public class datetime {
+	public static void main(String[] args) {
+		Date date=new Date();
+		System.out.print(date.toString());
+		
+	}
+
+}
